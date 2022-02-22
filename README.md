c182bb4b2625a9a8c3c148384b97a804a04f01a8
yangwang000 <wymm.010@gmail.com>
2022-02-22 10:48:30 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,55 @@
+## Groovy
+
+Playground: https://groovyconsole.appspot.com
+
+Example:
+```
+import groovy.json.JsonSlurper
+
+def object = new JsonSlurper().parseText(
+    '''
+    {
+    "workspaces": [
+        {
+            "id": "0b033d72-13c1-4dd6-8c3a-0f72aa165a36",
+            "name": "My Workspace",
+            "type": "personal"
+        }
+    ]
+}
+    '''
+)
+
+def query = object.workspaces.type
+```
+
+## Hamcrest Matchers
+### Collection matchers (List, Array, Map, etc.)
+hasItem() -> check single element in a collection
+not(hasItem()) -> check single element is NOT in a collection
+hasItems() -> Check all elements are in a collection
+contains() -> Check all elements are in a collection and in a strict order
+containsInAnyOrder() -> Check all elements are in a collection and in any order
+empty() -> Check if collection is empty
+not(emptyArray()) -> Check if the Array is not empty
+hasSize() -> Check size of a collection
+everyItem(startsWith()) -> Check if every item in a collection starts with specified string
+
+hasKey() -> Map -> Check if Map has the specified key [value is not checked]
+hasValue() -> Map -> Check if Map has at least one key matching specified value
+hasEntry() -> Maps -> Check if Map has the specified key value pair
+equalTo(Collections.EMPTY_MAP) -> Maps [Check if empty]
+allOf() -> Matches if all matchers matches
+anyOf() -> Matches if any of the matchers matches
+
+### Numbers:
+greaterThanOrEqualTo()
+lessThan()
+lessThanOrEqualTo()
+
+### String:
+containsString()
+emptyString()
+
+## HTTP Headers
+https://www.iana.org/assignments/message-headers/message-headers.xhtml
---
+++ src/test/java/com/rest/AutomateGet.java
@@ -0,0 +1,140 @@
+package com.rest;
+
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.Collections;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class AutomateGet {
+    private static final String apiKey = "PMAK-6214485830838164fab816cf-5b880c9e18cd341c46697cc9a5b9273f02";
+
+    @Test
+    public void validate_status_code(){
+        given().
+                baseUri("https://api.postman.com").
+                header("X-Api-Key", apiKey).
+        when().
+                get("/workspaces").
+        then().
+                log().all().
+                assertThat().
+                statusCode(200);
+    }
+
+   @Test
+    public void validate_response_body(){
+        given().
+                baseUri("https://api.postman.com").
+                header("X-Api-Key", apiKey).
+        when().
+                get("/workspaces").
+        then().
+                log().all().
+                assertThat().
+                statusCode(200).
+                body("workspaces.name", hasItems("Team Workspace", "My Workspace", "My Workspace2"),
+                        "workspaces.type", hasItems("team", "personal", "personal"),
+                        "workspaces[0].name", equalTo("Team Workspace"),
+                        "workspaces[0].name", is(equalTo("Team Workspace")),
+                        "workspaces.size()", equalTo(3),
+                        "workspaces.name", hasItem("Team Workspace")
+                );
+    }
+
+    @Test
+    public void validate_response_body_hamcrest_learnings(){
+        given().
+                baseUri("https://api.postman.com").
+                header("X-Api-Key", apiKey).
+        when().
+                get("/workspaces").
+        then().
+ //               log().all().
+                assertThat().
+                statusCode(200).
+                body("workspaces.name", containsInAnyOrder("Team Workspace", "My Workspace2", "My Workspace"),
+                        "workspaces.name", is(not(emptyArray())),
+                        "workspaces.name", hasSize(3),
+ //                       "workspaces.name", everyItem(startsWith("My"))
+                        "workspaces[0]", hasKey("id"),
+                        "workspaces[0]", hasValue("Team Workspace"),
+                        "workspaces[0]", hasEntry("id", "52bfb725-5a1a-4c38-8c05-24343951d389"),
+                        "workspaces[0]", not(equalTo(Collections.EMPTY_MAP)),
+                        "workspaces[0].name", allOf(startsWith("Team"), containsString("Workspace"))
+                );
+    }
+
+    @Test
+    public void extract_response(){
+        Response res = given().
+                baseUri("https://api.postman.com").
+                header("X-Api-Key", apiKey).
+        when().
+                get("/workspaces").
+        then().
+                assertThat().
+                statusCode(200).
+                extract().
+                response();
+        System.out.println("response = " + res.asString());
+    }
+
+    @Test
+    public void extract_single_value_from_response(){
+//        String name = given().
+//                baseUri("https://api.postman.com").
+//                header("X-Api-Key", apiKey).
+//        when().
+//                get("/workspaces").
+//        then().
+//                assertThat().
+//                statusCode(200).
+//                extract().
+//                response().path("workspaces[0].name");
+//        System.out.println("workspace name = " + name);
+
+
+        Response res = given().
+                baseUri("https://api.postman.com").
+                header("X-Api-Key", apiKey).
+                when().
+                get("/workspaces").
+                then().
+                assertThat().
+                statusCode(200).
+                extract().
+                response();
+        JsonPath jsonPath = new JsonPath(res.asString());
+        System.out.println("workspace name = " + JsonPath.from(res.asString()).getString("workspaces[0].name"));
+        System.out.println("workspace name = " + jsonPath.getString("workspaces[0].name"));
+        System.out.println("workspace name = " + res.path("workspaces[0].name"));
+    }
+
+    @Test
+    public void hamcrest_assert_on_extracted_response(){
+        String name = given().
+                baseUri("https://api.postman.com").
+                header("X-Api-Key", apiKey).
+        when().
+                get("/workspaces").
+        then().
+                assertThat().
+                statusCode(200).
+                extract().
+                response().path("workspaces[0].name");
+        System.out.println("workspace name = " + name);
+
+        assertThat(name, equalTo("Team Workspace1"));
+        Assert.assertEquals(name, "Team Workspace1");
+        //   System.out.println("workspace name = " + JsonPath.from(res).getString("workspaces[0].name"));
+        //    System.out.println("workspace name = " + jsonPath.getString("workspaces[0].name"));
+        //    System.out.println("workspace name = " + res.path("workspaces[0].name"));
+    }
+}
